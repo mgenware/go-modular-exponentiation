26801f1146e3b6d8fe3f5dd1458c95207a9a53bd
Mgen <m@mgenware.com>
2016-10-12 17:29:43 +0800

    More benchmark

--- main_test.go
+++ main_test.go
@@ -13,3 +13,15 @@ func BenchmarkModExpWithSquaring(b *testing.B) {
 		ModExpWithSquaring(123, 456, 89)
 	}
 }
+
+func BenchmarkModExpGoBigInteger(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ModExpGoBigInteger(123, 456, 89)
+	}
+}
+
+func BenchmarkModExpGoBigIntegerExp(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ModExpGoBigIntegerExp(123, 456, 89)
+	}
+}
